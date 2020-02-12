a57a8a97e2fbcb739c3c5142e76c3dace8fc0f0b
gvanderberg <gvanderberg@titansoftware.co.za>
2020-02-12 18:43:39 +0200

    add dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
-apt-get update
+AZ_REPO=$(lsb_release -cs)
 
 echo
 echo 1\) Install Docker
 echo
 
-# curl -sSL https://get.docker.com | bash
-apt-get install -y docker.io
+apt-get update && \
+    apt-get install -y docker.io
+rm -rf /var/lib/apt/lists/*
+
 cat <<EOF >/etc/docker/daemon.json
 {
     "bip": "10.0.60.1/24",
     "exec-opts": ["native.cgroupdriver=systemd"]
 }
 EOF
+
 systemctl restart docker
 systemctl enable docker
-usermod $USER -aG docker
+
+usermod azuresupport -aG docker
 
 echo
 echo 2\) Disable Swap
 echo
 
 swapoff -a
+
+echo
+echo 3\) Install dependencies
+echo
+
+apt-get update && \
+    apt-get install -y apt-transport-https openssh-server
+rm -rf /var/lib/apt/lists/*
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-$AZ_REPO main" > /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update && \
+    apt-get install -y kubeadm #kubelet kubectl
+rm -rf /var/lib/apt/lists/*
+rm -rf /etc/apt/sources.list.d/*
+
+apt-mark hold kubeadm #kubelet kubectl
