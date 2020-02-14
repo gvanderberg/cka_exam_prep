cbfc7f55c4d7f65b489bd3281c4d5921fa8df1da
gvanderberg <gvanderberg@titansoftware.co.za>
2020-02-14 21:51:36 +0200

    update allocation_method to static

--- tf/modules/vm/main.tf
+++ tf/modules/vm/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_public_ip" "this" {
   name                = format("%s-pip-%s", format("%s%s", var.virtual_machine_name, count.index + 1), random_integer.this[count.index].result)
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
   tags                = var.tags
 }
