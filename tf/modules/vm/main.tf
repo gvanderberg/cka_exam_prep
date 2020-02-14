bb3fb78dba77392e03991e5035b7d395a69dbe2d
gvanderberg <gvanderberg@titansoftware.co.za>
2020-02-14 21:44:26 +0200

    update vm pip sku

--- tf/modules/vm/main.tf
+++ tf/modules/vm/main.tf
@@ -10,6 +10,7 @@ resource "azurerm_public_ip" "this" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
+  sku                 = "Standard"
   tags                = var.tags
 }
 
