2a692823cc02e7257febea2cf368ac8fdbcae0c7
gvanderberg <gvanderberg@titansoftware.co.za>
2020-02-14 20:24:41 +0200

    update

--- tf/modules/lb/main.tf
+++ tf/modules/lb/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "this" {
-  name                = "azlb-dev-zn-k8s-api-pip"
+  name                = "kubernetes-pip"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
@@ -8,7 +8,7 @@ resource "azurerm_public_ip" "this" {
 }
 
 resource "azurerm_lb" "this" {
-  name                = "azlb-dev-zn-k8s-api"
+  name                = "kubernetes-lb"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   sku                 = "Standard"
@@ -22,7 +22,7 @@ resource "azurerm_lb" "this" {
 }
 
 resource "azurerm_lb_backend_address_pool" "this" {
+  name                = "kubernetes-lb-pool"
   resource_group_name = var.resource_group_name
   loadbalancer_id     = azurerm_lb.this.id
-  name                = "BackEndAddressPool"
 }
--- tf/modules/nsg/main.tf
+++ tf/modules/nsg/main.tf
@@ -1,16 +1,28 @@
 resource "azurerm_network_security_group" "this" {
-  name                = "aznsg-dev-zn-k8s"
+  name                = "aznsg-dev-zn-kubernetes"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
   security_rule {
-    name                       = "default-allow-ssh"
+    name                       = "kubernetes-allow-ssh"
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "kubernetes-allow-https"
+    priority                   = 1100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
