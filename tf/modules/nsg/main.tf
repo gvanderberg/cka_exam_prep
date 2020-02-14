2d5c253c35a7ff6a45f8da617f5df2ef35ed27b1
gvanderberg <gvanderberg@titansoftware.co.za>
2020-02-14 20:30:22 +0200

    update nsg name

--- tf/modules/nsg/main.tf
+++ tf/modules/nsg/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "this" {
-  name                = "aznsg-dev-zn-kubernetes"
+  name                = "kubernetes-nsg"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 
