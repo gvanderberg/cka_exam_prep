20d4cf9bfbdfb5cd3cb447620252183fffe12a3f
Gareth van der Berg <gareth.shaun@gmail.com>
2020-02-16 12:42:26 +0200

    Update config-master.sh

--- scripts/config-master.sh
+++ scripts/config-master.sh
@@ -5,10 +5,9 @@ kubeadm config images pull -v3
 kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=NumCPU
 
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
 
 curl https://docs.projectcalico.org/manifests/calico.yaml -O
 
 kubectl apply -f calico.yaml
-
